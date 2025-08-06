659ba805b92554c5def85d394d502d355bec743f
Anderson Vieira <asvieira.dev@gmail.com>
2025-08-06 02:10:04 -0300

    refactor: include error messages

--- src/printer/domain/entities/value-objects/__tests__/ipv4.spec.ts
+++ src/printer/domain/entities/value-objects/__tests__/ipv4.spec.ts
@@ -16,27 +16,39 @@ describe('IPV4 Value Object', () => {
   });
 
   it('shoud throw MissingParamException if IP is not provided', () => {
-    expect(() => IPV4.create('')).toThrow(MissingParamException);
+    expect(() => IPV4.create('')).toThrow(new MissingParamException('IP não informado.'));
   });
 
   it('should throw InvalidParamException if IP has less than 4 digits', () => {
-    expect(() => IPV4.create('127.0.0')).toThrow(InvalidParamException);
+    expect(() => IPV4.create('127.0.0')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
   });
 
   it('should throw InvalidParamException if IP has more than 4 groups of digits', () => {
-    expect(() => IPV4.create('192.168.0.1.5')).toThrow(InvalidParamException);
+    expect(() => IPV4.create('192.168.0.1.5')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
   });
 
   it('should throw InvalidParamException if any group of digits has more than 3 digits', () => {
-    expect(() => IPV4.create('192.300.0.1')).toThrow(InvalidParamException);
+    expect(() => IPV4.create('192.300.0.1')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
   });
 
   it('should throw InvalidParamException if any group of digits is not numeric', () => {
-    expect(() => IPV4.create('abc.def.ghi.jkl')).toThrow(InvalidParamException);
-    expect(() => IPV4.create('192.168.x.1')).toThrow(InvalidParamException);
+    expect(() => IPV4.create('abc.def.ghi.jkl')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
+    expect(() => IPV4.create('192.168.x.1')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
   });
 
   it('should throw InvalidParamException if any group of digits is empty', () => {
-    expect(() => IPV4.create('192..0.1')).toThrow(InvalidParamException);
+    expect(() => IPV4.create('192..0.1')).toThrow(
+      new InvalidParamException('IP inválido. Formato esperado: xxx.xxx.xxx.xxx'),
+    );
   });
 });
