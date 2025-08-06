b3ad14e5e6aa7a8ca1b702920cdc1bc0e22254a7
Anderson Vieira <asvieira.dev@gmail.com>
2025-08-05 22:39:27 -0300

    refactor: formating file columns

--- src/printer/domain/entities/__tests__/model.spec.ts
+++ src/printer/domain/entities/__tests__/model.spec.ts
@@ -3,7 +3,12 @@ import { Model } from '../model';
 
 describe('Model entity', () => {
   it('should create a new Model with correct params', () => {
-    const newModel = Model.Create('Kyocera', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11');
+    const newModel = Model.Create(
+      'Kyocera',
+      'KM2040DN',
+      '1.2.1.2.3.5.6.7.41.10',
+      '1.2.1.2.3.5.6.7.41.11',
+    );
 
     expect(newModel).toBeInstanceOf(Model);
     expect(newModel.Id).toBeDefined();
@@ -16,47 +21,56 @@ describe('Model entity', () => {
   });
 
   it('should throw MissingParamException if Manufacturer is not provided', () => {
-    expect(() => Model.Create('', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('Nome do fabricante não informado.'))
-  })
+    expect(() =>
+      Model.Create('', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new MissingParamException('Nome do fabricante não informado.'));
+  });
 
   it('should throw MissingParamException if Manufacturer is null', () => {
-    expect(() => Model.Create(null as any, 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('Nome do fabricante não informado.'))
-  })
+    expect(() =>
+      Model.Create(null as any, 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new MissingParamException('Nome do fabricante não informado.'));
+  });
 
   it('should throw InvalidParamException if Manufacturer name is less than 3 characters', () => {
-    expect(() => Model.Create('Ky', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new InvalidParamException('Nome do fabricante deve conter no mínimo 3 caracteres.'))
-  })
+    expect(() =>
+      Model.Create('Ky', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new InvalidParamException('Nome do fabricante deve conter no mínimo 3 caracteres.'));
+  });
 
   it('should throw MissingParamException if Description is not provided', () => {
-    expect(() => Model.Create('Kyocera', '', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('Descrição do modelo não informado.'))
-  })
+    expect(() =>
+      Model.Create('Kyocera', '', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new MissingParamException('Descrição do modelo não informado.'));
+  });
 
   it('should throw MissingParamException if Description is null', () => {
-    expect(() => Model.Create('Kyocera', null as any, '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('Descrição do modelo não informado.'))
-  })
+    expect(() =>
+      Model.Create('Kyocera', null as any, '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new MissingParamException('Descrição do modelo não informado.'));
+  });
 
   it('should throw InvalidParamException if Description is less than 3 characters', () => {
-    expect(() => Model.Create('Kyocera', 'KM', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new InvalidParamException('Descrição do modelo deve conter no mínimo 3 caracteres.'))
-  })
+    expect(() =>
+      Model.Create('Kyocera', 'KM', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
+    ).toThrow(new InvalidParamException('Descrição do modelo deve conter no mínimo 3 caracteres.'));
+  });
 
   it('should throw MissingParamException if Description is not provided', () => {
-    expect(() => Model.Create('Kyocera', 'KM2040DN', '', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('OID de contador não informado.'))
-  })
+    expect(() => Model.Create('Kyocera', 'KM2040DN', '', '1.2.1.2.3.5.6.7.41.11')).toThrow(
+      new MissingParamException('OID de contador não informado.'),
+    );
+  });
 
   it('should throw MissingParamException if Description is null', () => {
-    expect(() => Model.Create('Kyocera', 'KM2040DN', null as any, '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new MissingParamException('OID de contador não informado.'))
-  })
+    expect(() => Model.Create('Kyocera', 'KM2040DN', null as any, '1.2.1.2.3.5.6.7.41.11')).toThrow(
+      new MissingParamException('OID de contador não informado.'),
+    );
+  });
 
   it('should throw InvalidParamException if Description is less than 10 characters', () => {
-    expect(() => Model.Create('Kyocera', 'KM2040DN', '1.2.3.4.5', '1.2.1.2.3.5.6.7.41.11'))
-      .toThrow(new InvalidParamException('OID de contador deve conter no mínimo 10 caracteres.'))
-  })
+    expect(() => Model.Create('Kyocera', 'KM2040DN', '1.2.3.4.5', '1.2.1.2.3.5.6.7.41.11')).toThrow(
+      new InvalidParamException('OID de contador deve conter no mínimo 10 caracteres.'),
+    );
+  });
 });
--- src/printer/domain/entities/model.ts
+++ src/printer/domain/entities/model.ts
@@ -2,11 +2,14 @@ import { UnprocessableEntityException } from '@nestjs/common';
 import { EntityBase } from './entity-base';
 import { InvalidParamException, MissingParamException } from '../exceptions';
 
-const InvalidManufacturerExceptionMessage = 'Nome do fabricante deve conter no mínimo 3 caracteres.';
+const InvalidManufacturerExceptionMessage =
+  'Nome do fabricante deve conter no mínimo 3 caracteres.';
 const MissingManufacturerExceptionMessage = 'Nome do fabricante não informado.';
-const InvalidDescriptionExceptionMessage = 'Descrição do modelo deve conter no mínimo 3 caracteres.';
+const InvalidDescriptionExceptionMessage =
+  'Descrição do modelo deve conter no mínimo 3 caracteres.';
 const MissingDescriptionExceptionMessage = 'Descrição do modelo não informado.';
-const InvalidOidExceptionMessage = 'OID de contador inválido ou não informado.';
+const InvalidOidExceptionMessage = 'OID de contador deve conter no mínimo 10 caracteres.';
+const MissingOidExceptionMessage = 'OID de contador não informado.';
 
 export class Model extends EntityBase {
   readonly Manufacturer: string;
@@ -14,7 +17,12 @@ export class Model extends EntityBase {
   readonly OidPrint: string;
   readonly Oidcopy: string;
 
-  private constructor(manufacturer: string, description: string, oidPrint: string, oidcopy: string) {
+  private constructor(
+    manufacturer: string,
+    description: string,
+    oidPrint: string,
+    oidcopy: string,
+  ) {
     super();
     this.Manufacturer = manufacturer;
     this.Description = description;
@@ -23,25 +31,30 @@ export class Model extends EntityBase {
     this.Validate();
   }
 
-  public static Create(manufacturer: string, description: string, oidPrint: string, oidcopy: string): Model {
+  public static Create(
+    manufacturer: string,
+    description: string,
+    oidPrint: string,
+    oidcopy: string,
+  ): Model {
     return new Model(manufacturer, description, oidPrint, oidcopy);
   }
 
   private Validate() {
-    if (!this.Manufacturer)
-      throw new MissingParamException(MissingManufacturerExceptionMessage);
-    
+    if (!this.Manufacturer) throw new MissingParamException(MissingManufacturerExceptionMessage);
+
     if (this.Manufacturer.trim().length < 3)
       throw new InvalidParamException(InvalidManufacturerExceptionMessage);
 
-    if (!this.Description)
-      throw new MissingParamException(MissingDescriptionExceptionMessage);
+    if (!this.Description) throw new MissingParamException(MissingDescriptionExceptionMessage);
 
-     if (this.Description.trim().length < 3)
+    if (this.Description.trim().length < 3)
       throw new InvalidParamException(InvalidDescriptionExceptionMessage);
 
-    if (!this.OidPrint || this.OidPrint.trim().length < 10)
-      throw new UnprocessableEntityException(InvalidOidExceptionMessage);
+    if (!this.OidPrint) throw new MissingParamException(MissingOidExceptionMessage);
+
+    if (this.OidPrint.trim().length < 10)
+      throw new InvalidParamException(InvalidOidExceptionMessage);
 
     if (!this.Oidcopy || this.Oidcopy.trim().length < 10)
       throw new UnprocessableEntityException(InvalidOidExceptionMessage);
