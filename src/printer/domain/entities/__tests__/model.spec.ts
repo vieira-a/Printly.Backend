a6e7c0dae6857e265626925455969b941413a8f9
Anderson Vieira <asvieira.dev@gmail.com>
2025-08-07 00:53:29 -0300

    fix: imports to ModelDomainValidationException

--- src/printer/domain/entities/__tests__/model.spec.ts
+++ src/printer/domain/entities/__tests__/model.spec.ts
@@ -1,5 +1,5 @@
+import { ModelDomainValidationException } from '@printer/domain/exceptions';
 import { Model } from '../model';
-import { DomainValidationException } from '@shared/exceptions/domain-validation.exception';
 
 describe('Model Entity', () => {
   it('should create a new Model with correct params', () => {
@@ -20,75 +20,75 @@ describe('Model Entity', () => {
     expect(newModel.updatedAt).toBeInstanceOf(Date);
   });
 
-  it('should throw DomainValidationException if Manufacturer is not provided', () => {
+  it('should throw ModelDomainValidationException if Manufacturer is not provided', () => {
     expect(() =>
       Model.create('', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if Manufacturer is null', () => {
+  it('should throw ModelDomainValidationException if Manufacturer is null', () => {
     expect(() =>
       Model.create(null as any, 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if Manufacturer name is less than 3 characters', () => {
+  it('should throw ModelDomainValidationException if Manufacturer name is less than 3 characters', () => {
     expect(() =>
       Model.create('Ky', 'KM2040DN', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if Description is not provided', () => {
+  it('should throw ModelDomainValidationException if Description is not provided', () => {
     expect(() =>
       Model.create('Kyocera', '', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if Description is null', () => {
+  it('should throw ModelDomainValidationException if Description is null', () => {
     expect(() =>
       Model.create('Kyocera', null as any, '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if Description is less than 3 characters', () => {
+  it('should throw ModelDomainValidationException if Description is less than 3 characters', () => {
     expect(() =>
       Model.create('Kyocera', 'KM', '1.2.1.2.3.5.6.7.41.10', '1.2.1.2.3.5.6.7.41.11'),
-    ).toThrow(DomainValidationException);
+    ).toThrow(ModelDomainValidationException);
   });
 
-  it('should throw DomainValidationException if PrintOid is not provided', () => {
+  it('should throw ModelDomainValidationException if PrintOid is not provided', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', '', '1.2.1.2.3.5.6.7.41.11')).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 
-  it('should throw DomainValidationException if PrintOid is null', () => {
+  it('should throw ModelDomainValidationException if PrintOid is null', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', null as any, '1.2.1.2.3.5.6.7.41.11')).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 
-  it('should throw DomainValidationException if PrintOid is less than 10 characters', () => {
+  it('should throw ModelDomainValidationException if PrintOid is less than 10 characters', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', '1.2.3.4.5', '1.2.1.2.3.5.6.7.41.11')).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 
-  it('should throw DomainValidationException if CopyOid is not provided', () => {
+  it('should throw ModelDomainValidationException if CopyOid is not provided', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', '', '1.2.1.2.3.5.6.7.41.11')).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 
-  it('should throw DomainValidationException if CopyOid is null', () => {
+  it('should throw ModelDomainValidationException if CopyOid is null', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', '1.2.1.2.3.5.6.7.41.11', null as any)).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 
-  it('should throw DomainValidationException if CopyOid is less than 10 characters', () => {
+  it('should throw ModelDomainValidationException if CopyOid is less than 10 characters', () => {
     expect(() => Model.create('Kyocera', 'KM2040DN', '1.2.1.2.3.5.6.7.41.11', '1.2.3.4.5')).toThrow(
-      DomainValidationException,
+      ModelDomainValidationException,
     );
   });
 });
