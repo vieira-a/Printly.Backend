53500ed78620840d339845367671ad0530d84b69
Anderson Vieira <asvieira.dev@gmail.com>
2025-08-15 10:13:18 -0300

    test: add test to retrieve a list of countings

--- src/printer-management/domain/entities/__tests__/printer.spec.ts
+++ src/printer-management/domain/entities/__tests__/printer.spec.ts
@@ -17,86 +17,127 @@ const validPrinterProps: CreatePrinterProps = {
 const validPrinter = Printer.create({ ...validPrinterProps });
 
 describe('Printer Entity', () => {
-  it('should throw a PrinterDomainValidationException if serialNumber is not provided', () => {
-    expect(() => Printer.create({ ...validPrinterProps, serialNumber: '' })).toThrow(
-      PrinterDomainValidationException,
-    );
+  describe('Validation', () => {
+    describe('Create', () => {
+      it('should throw a PrinterDomainValidationException if serialNumber is not provided', () => {
+        expect(() => Printer.create({ ...validPrinterProps, serialNumber: '' })).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if serialNumber has less than 6 characters', () => {
+        expect(() => Printer.create({ ...validPrinterProps, serialNumber: 'XYZ' })).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if installationLocationId is not provided', () => {
+        expect(() => Printer.create({ ...validPrinterProps, installationLocationId: '' })).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if installationLocationId is not provided', () => {
+        expect(() => Printer.create({ ...validPrinterProps, installedAt: null as any })).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if modelId is not provided', () => {
+        expect(() => Printer.create({ ...validPrinterProps, modelId: '' })).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if new total print is less than current total print', () => {
+        expect(() =>
+          validPrinter.addCounting(
+            'fake-counting-job-id',
+            CountingType.MANUAL,
+            999,
+            9999,
+            new Date(),
+          ),
+        ).toThrow(PrinterDomainValidationException);
+      });
+
+      it('should throw a PrinterDomainValidationException if new total copy is less than current total copy', () => {
+        expect(() =>
+          validPrinter.addCounting(
+            'fake-counting-job-id',
+            CountingType.MANUAL,
+            9999,
+            999,
+            new Date(),
+          ),
+        ).toThrow(PrinterDomainValidationException);
+      });
+
+      it('should create a new Printer with correct params', () => {
+        expect(validPrinter).toBeInstanceOf(Printer);
+        expect(validPrinter.serialNumber).toBe('XYZ12345');
+        expect(validPrinter.ipv4Address.toString()).toBe('192.168.0.200');
+        expect(validPrinter.modelId).toBe('fake-model-id');
+        expect(validPrinter.installationLocationId).toBe('fake-location-id');
+      });
+    });
+
+    describe('Update', () => {
+      it('should throw a PrinterDomainValidationException if serialNumber is not provided on update', () => {
+        expect(() => validPrinter.updateSerialNumber('')).toThrow(PrinterDomainValidationException);
+      });
+
+      it('should throw a PrinterDomainValidationException if model id is not provided on update', () => {
+        expect(() => validPrinter.updateModel('')).toThrow(PrinterDomainValidationException);
+      });
+
+      it('should throw a PrinterDomainValidationException if installation location id is not provided on update', () => {
+        expect(() => validPrinter.updateInstallationLocation('', new Date())).toThrow(
+          PrinterDomainValidationException,
+        );
+      });
+
+      it('should throw a PrinterDomainValidationException if installation date is not provided on update', () => {
+        expect(() =>
+          validPrinter.updateInstallationLocation('fake-installation-location-id', null as any),
+        ).toThrow(PrinterDomainValidationException);
+      });
+    });
   });
 
-  it('should throw a PrinterDomainValidationException if serialNumber has less than 6 characters', () => {
-    expect(() => Printer.create({ ...validPrinterProps, serialNumber: 'XYZ' })).toThrow(
-      PrinterDomainValidationException,
-    );
-  });
+  describe('Counting', () => {
+    it('should addCounting with correct params', () => {
+      const newCounting = validPrinter.addCounting(
+        'fake-counting-job-id',
+        CountingType.MANUAL,
+        9999,
+        9999,
+        new Date(),
+      );
+      expect(validPrinter.countings).toContain(newCounting);
+      expect(validPrinter.totalPrint).toBe(9999);
+      expect(validPrinter.totalCopy).toBe(9999);
+    });
 
-  it('should throw a PrinterDomainValidationException if installationLocationId is not provided', () => {
-    expect(() => Printer.create({ ...validPrinterProps, installationLocationId: '' })).toThrow(
-      PrinterDomainValidationException,
-    );
-  });
+    it('should retrieve counting successfully', () => {
+      const newCounting1 = validPrinter.addCounting(
+        'fake-counting-job-id-1',
+        CountingType.MANUAL,
+        9999,
+        9999,
+        new Date(),
+      );
 
-  it('should throw a PrinterDomainValidationException if installationLocationId is not provided', () => {
-    expect(() => Printer.create({ ...validPrinterProps, installedAt: null as any })).toThrow(
-      PrinterDomainValidationException,
-    );
-  });
+      const newCounting2 = validPrinter.addCounting(
+        'fake-counting-job-id-2',
+        CountingType.AUTO,
+        99991,
+        99991,
+        new Date(),
+      );
 
-  it('should throw a PrinterDomainValidationException if modelId is not provided', () => {
-    expect(() => Printer.create({ ...validPrinterProps, modelId: '' })).toThrow(
-      PrinterDomainValidationException,
-    );
-  });
-
-  it('should throw a PrinterDomainValidationException if new total print is less than current total print', () => {
-    expect(() =>
-      validPrinter.addCounting('fake-counting-job-id', CountingType.MANUAL, 999, 9999, new Date()),
-    ).toThrow(PrinterDomainValidationException);
-  });
-
-  it('should throw a PrinterDomainValidationException if new total copy is less than current total copy', () => {
-    expect(() =>
-      validPrinter.addCounting('fake-counting-job-id', CountingType.MANUAL, 9999, 999, new Date()),
-    ).toThrow(PrinterDomainValidationException);
-  });
-
-  it('should create a new Printer with correct params', () => {
-    expect(validPrinter).toBeInstanceOf(Printer);
-    expect(validPrinter.serialNumber).toBe('XYZ12345');
-    expect(validPrinter.ipv4Address.toString()).toBe('192.168.0.200');
-    expect(validPrinter.modelId).toBe('fake-model-id');
-    expect(validPrinter.installationLocationId).toBe('fake-location-id');
-  });
-
-  it('should addCounting with correct params', () => {
-    const newCounting = validPrinter.addCounting(
-      'fake-counting-job-id',
-      CountingType.MANUAL,
-      9999,
-      9999,
-      new Date(),
-    );
-    expect(validPrinter.countings).toContain(newCounting);
-    expect(validPrinter.totalPrint).toBe(9999);
-    expect(validPrinter.totalCopy).toBe(9999);
-  });
-
-  it('should throw a PrinterDomainValidationException if serialNumber is not provided on update', () => {
-    expect(() => validPrinter.updateSerialNumber('')).toThrow(PrinterDomainValidationException);
-  });
-
-  it('should throw a PrinterDomainValidationException if model id is not provided on update', () => {
-    expect(() => validPrinter.updateModel('')).toThrow(PrinterDomainValidationException);
-  });
-
-  it('should throw a PrinterDomainValidationException if installation location id is not provided on update', () => {
-    expect(() => validPrinter.updateInstallationLocation('', new Date())).toThrow(
-      PrinterDomainValidationException,
-    );
-  });
-
-  it('should throw a PrinterDomainValidationException if installation date is not provided on update', () => {
-    expect(() =>
-      validPrinter.updateInstallationLocation('fake-installation-location-id', null as any),
-    ).toThrow(PrinterDomainValidationException);
+      expect(validPrinter.countings).toContain(newCounting1);
+      expect(validPrinter.countings).toContain(newCounting2);
+    });
   });
 });
